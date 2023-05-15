3227238e0bbb62e814e3ad00b6a32465e07796c0
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-05-15 05:14:40 +0200

    implement jnoise pipeline for algorythme

--- src/main/java/fr/iamacat/iamacatblockgame/Main.java
+++ src/main/java/fr/iamacat/iamacatblockgame/Main.java
@@ -1,31 +1,57 @@
 package fr.iamacat.iamacatblockgame;
 
 import fr.iamacat.iamacatblockgame.gamescreen.GameWindow;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWMouseButtonCallback;
 
 public class Main {
 
-    private GameWindow window;
+    private long window;
+    private boolean showTitleScreen;
 
     public void run() {
         init();
+        showTitleScreen = true;
         loop();
         cleanup();
     }
 
     private void init() {
-        window = new GameWindow(1280, 720, "IamACat Block Game");
-        window.setWindowIcon("resources/textures/gamescreen/icon.png");
+        // Initialize GLFW and create the window
+        GLFW.glfwInit();
+        window = GLFW.glfwCreateWindow(1280, 720, "IamACat Block Game", 0, 0);
+        GLFW.glfwMakeContextCurrent(window);
+        GLFW.glfwSwapInterval(1);
+
+        // Set up mouse button callback for handling clicks
+        GLFW.glfwSetMouseButtonCallback(window, new GLFWMouseButtonCallback() {
+            @Override
+            public void invoke(long window, int button, int action, int mods) {
+                if (showTitleScreen && action == GLFW.GLFW_PRESS) {
+                    // Launch the game here
+                    System.out.println("Launching the game...");
+                    showTitleScreen = false;
+                }
+            }
+        });
     }
 
     private void loop() {
-        while (!window.shouldClose()) {
-            // Perform game logic and rendering
-            window.update();
+        while (!GLFW.glfwWindowShouldClose(window) && showTitleScreen) {
+            // Perform game logic and rendering for the title screen
+            GLFW.glfwPollEvents();
+        }
+
+        while (!GLFW.glfwWindowShouldClose(window)) {
+            // Perform game logic and rendering for the main game
+            GLFW.glfwPollEvents();
         }
     }
 
     private void cleanup() {
-        window.close();
+        // Clean up GLFW resources
+        GLFW.glfwDestroyWindow(window);
+        GLFW.glfwTerminate();
     }
 
     public static void main(String[] args) {
