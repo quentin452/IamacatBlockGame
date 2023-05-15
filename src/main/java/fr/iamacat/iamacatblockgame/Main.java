73deb6e4d667ce40ab726cf363c6136805304147
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-05-15 04:03:04 +0200

    try to make a game icon

--- src/main/java/fr/iamacat/iamacatblockgame/Main.java
+++ src/main/java/fr/iamacat/iamacatblockgame/Main.java
@@ -1,12 +1,10 @@
 package fr.iamacat.iamacatblockgame;
 
-import org.lwjgl.glfw.GLFW;
-import org.lwjgl.opengl.GL;
-import org.lwjgl.opengl.GL11;
+import fr.iamacat.iamacatblockgame.gamescreen.GameWindow;
 
 public class Main {
 
-    private long window;
+    private GameWindow window;
 
     public void run() {
         init();
@@ -15,40 +13,19 @@ public class Main {
     }
 
     private void init() {
-        GLFW.glfwInit();
-
-        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 4);
-        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 6);
-        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
-        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GLFW.GLFW_TRUE);
-
-        window = GLFW.glfwCreateWindow(800, 600, "LWJGL Game", 0, 0);
-        if (window == 0) {
-            throw new RuntimeException("Failed to create the GLFW window");
-        }
-
-        GLFW.glfwMakeContextCurrent(window);
-        GLFW.glfwShowWindow(window);
-
-        GL.createCapabilities();
-
-        GL11.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        window = new GameWindow(1280, 720, "IamACat Block Game");
+        window.setWindowIcon("resources/textures/gamescreen/icon.png");
     }
 
     private void loop() {
-        while (!GLFW.glfwWindowShouldClose(window)) {
-            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
-
-            // Render your game here using Modern OpenGL API
-
-            // Swap buffers and poll events
-            GLFW.glfwSwapBuffers(window);
-            GLFW.glfwPollEvents();
+        while (!window.shouldClose()) {
+            // Perform game logic and rendering
+            window.update();
         }
     }
 
     private void cleanup() {
-        GLFW.glfwTerminate();
+        window.close();
     }
 
     public static void main(String[] args) {
