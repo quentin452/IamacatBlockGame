81c84352cce1772aa1259ace740257bcb12a8d09
QUENTIN453 <quentinlamauve@gmail.com>
2023-05-16 20:20:45 +0200

    update

--- src/main/java/fr/iamacat/iamacatblockgame/renderer/Renderer.java
+++ src/main/java/fr/iamacat/iamacatblockgame/renderer/Renderer.java
@@ -15,6 +15,7 @@ public class Renderer {
     private static int texCoordVBOID;
     private static int indexVBOID;
     private static boolean isTitleScreenDisplayed = false;
+
     public static void renderTitleScreen(int textureID, int vaoIDParam) {
         GL46.glActiveTexture(GL46.GL_TEXTURE0);
         GL46.glBindTexture(GL46.GL_TEXTURE_2D, textureID);
@@ -26,7 +27,7 @@ public class Renderer {
         GL46.glEnableVertexAttribArray(0);
         GL46.glEnableVertexAttribArray(1);
 
-   //     GL46.glDrawElements(GL46.GL_TRIANGLES, 6, GL46.GL_UNSIGNED_INT, 0);
+        GL46.glDrawElements(GL46.GL_TRIANGLES, 6, GL46.GL_UNSIGNED_INT, 0);
 
         GL46.glDisableVertexAttribArray(0);
         GL46.glDisableVertexAttribArray(1);
@@ -37,12 +38,13 @@ public class Renderer {
 
         isTitleScreenDisplayed = true;
 
-       Renderer.deleteResources();
-        }
+        deleteResources();
+    }
 
     private static void bindTextureId(int textureId) {
         GL46.glBindTexture(GL46.GL_TEXTURE_2D, textureId);
-        }
+    }
+
     public static void renderButtons(List<Button> buttons) {
         enableBlending();
 
@@ -53,7 +55,7 @@ public class Renderer {
             GL46.glBindVertexArray(vaoID);
         }
 
-       // Create and bind the vertex and texture coordinate buffers outside the loop
+        // Create and bind the vertex and texture coordinate buffers outside the loop
         GL46.glBindBuffer(GL46.GL_ARRAY_BUFFER, vertexVBOID);
         GL46.glVertexAttribPointer(0, 3, GL46.GL_FLOAT, false, 0, 0);
 
@@ -76,7 +78,7 @@ public class Renderer {
             GL46.glEnableVertexAttribArray(0);
             GL46.glEnableVertexAttribArray(1);
 
-            //GL46.glDrawElements(GL46.GL_TRIANGLES, indicesBuffer);
+            GL46.glDrawElements(GL46.GL_TRIANGLES, indicesBuffer);
 
             GL46.glDisableVertexAttribArray(0);
             GL46.glDisableVertexAttribArray(1);
@@ -88,13 +90,12 @@ public class Renderer {
         disableBlending();
         isTitleScreenDisplayed = false;
 
-        Renderer.deleteResources();
-
-
+        deleteResources();
     }
 
     private static void createVAO() {
         vaoID = GL46.glGenVertexArrays();
+
         GL46.glBindVertexArray(vaoID);
 
         vertexVBOID = GL46.glGenBuffers();
@@ -129,6 +130,19 @@ public class Renderer {
         GL46.glBufferData(GL46.GL_ARRAY_BUFFER, texCoordsBuffer, GL46.GL_STATIC_DRAW);
         GL46.glVertexAttribPointer(1, 2, GL46.GL_FLOAT, false, 0, 0);
 
+        indexVBOID = GL46.glGenBuffers();
+        GL46.glBindBuffer(GL46.GL_ELEMENT_ARRAY_BUFFER, indexVBOID);
+
+        int[] indices = {
+                0, 1, 2,    // First triangle (top-right, top-left, bottom-right)
+                2, 3, 0     // Second triangle (bottom-right, bottom-left, top-left)
+        };
+
+        IntBuffer indicesBuffer = BufferUtils.createIntBuffer(indices.length);
+        indicesBuffer.put(indices).flip();
+
+        GL46.glBufferData(GL46.GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, indicesBuffer.capacity() * 4);
+
         GL46.glBindBuffer(GL46.GL_ARRAY_BUFFER, 0);
         GL46.glBindVertexArray(0);
     }
@@ -148,7 +162,7 @@ public class Renderer {
         }
     }
 
-   private static void deleteVAOAndVBOs() {
+    private static void deleteVAOAndVBOs() {
         if (vaoID != 0) {
             GL46.glDeleteVertexArrays(vaoID);
             vaoID = 0;
@@ -158,14 +172,14 @@ public class Renderer {
             vertexVBOID = 0;
         }
 
-      if (texCoordVBOID != 0) {
-        GL46.glDeleteBuffers(texCoordVBOID);
-        texCoordVBOID = 0;
-    }
+        if (texCoordVBOID != 0) {
+            GL46.glDeleteBuffers(texCoordVBOID);
+            texCoordVBOID = 0;
+        }
 
-    if (indexVBOID != 0) {
-        GL46.glDeleteBuffers(indexVBOID);
-        indexVBOID = 0;
+        if (indexVBOID != 0) {
+            GL46.glDeleteBuffers(indexVBOID);
+            indexVBOID = 0;
         }
     }
 }
