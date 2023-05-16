34b7681523c7b7d42ffe1b1925696f6748a4af7b
QUENTIN453 <quentinlamauve@gmail.com>
2023-05-16 14:14:07 +0200

    Update Renderer.java

--- src/main/java/fr/iamacat/iamacatblockgame/renderer/Renderer.java
+++ src/main/java/fr/iamacat/iamacatblockgame/renderer/Renderer.java
@@ -1,8 +1,11 @@
 package fr.iamacat.iamacatblockgame.renderer;
 
 import fr.iamacat.iamacatblockgame.gamescreen.Button;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL46;
 
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import java.util.List;
 
 public class Renderer {
@@ -11,14 +14,14 @@ public class Renderer {
     private static int texCoordVBOID;
     private static int indexVBOID;
     private static boolean isTitleScreenDisplayed = false;
-    public static void renderTitleScreen(int textureID, int vaoID) {
+public static void renderTitleScreen(int textureID, int vaoIDParam) {
         GL46.glActiveTexture(GL46.GL_TEXTURE0);
         GL46.glBindTexture(GL46.GL_TEXTURE_2D, textureID);
 
         GL46.glEnable(GL46.GL_BLEND);
         GL46.glBlendFunc(GL46.GL_SRC_ALPHA, GL46.GL_ONE_MINUS_SRC_ALPHA);
 
-        GL46.glBindVertexArray(vaoID);
+        GL46.glBindVertexArray(vaoIDParam);
         GL46.glEnableVertexAttribArray(0);
         GL46.glEnableVertexAttribArray(1);
 
@@ -34,37 +37,39 @@ public class Renderer {
         isTitleScreenDisplayed = true;
 
         Renderer.deleteResources();
-    }
-    private static void bindTextureId(int textureId) {
+        }
+
+private static void bindTextureId(int textureId) {
         GL46.glBindTexture(GL46.GL_TEXTURE_2D, textureId);
-    }
+        }
+
     public static void renderButtons(List<Button> buttons) {
         enableBlending();
 
         if (vaoID == 0) {
-            createVAO();
+        createVAO();
         } else {
-            GL46.glBindVertexArray(vaoID);
+        GL46.glBindVertexArray(vaoID);
         }
 
         for (Button button : buttons) {
-            float[] vertices = {
-                    // Define the button's vertices here
-                    // Example: x, y, z coordinates of each vertex
-                    -0.5f, 0.5f, 0.0f,    // Top-left vertex
-                    0.5f, 0.5f, 0.0f,     // Top-right vertex
-                    0.5f, -0.5f, 0.0f,    // Bottom-right vertex
-                    -0.5f, -0.5f, 0.0f    // Bottom-left vertex
-            };
+        float[] vertices = {
+        // Define the button's vertices here
+        // Example: x, y, z coordinates of each vertex
+        -0.5f, 0.5f, 0.0f,    // Top-left vertex
+        0.5f, 0.5f, 0.0f,     // Top-right vertex
+        0.5f, -0.5f, 0.0f,    // Bottom-right vertex
+        -0.5f, -0.5f, 0.0f    // Bottom-left vertex
+        };
 
-            float[] texCoords = {
-                    // Define the button's texture coordinates here
-                    // Example: u, v coordinates of each vertex
-                    0.0f, 1.0f,    // Top-left vertex
-                    1.0f, 1.0f,    // Top-right vertex
-                    1.0f, 0.0f,    // Bottom-right vertex
-                    0.0f, 0.0f     // Bottom-left vertex
-            };
+        float[] texCoords = {
+        // Define the button's texture coordinates here
+        // Example: u, v coordinates of each vertex
+        0.0f, 1.0f,    // Top-left vertex
+        1.0f, 1.0f,    // Top-right vertex
+                1.0f, 0.0f,    // Bottom-right vertex
+                0.0f, 0.0f     // Bottom-left vertex
+        };
 
             int[] indices = {
                     // Define the indices for the triangles here
@@ -73,19 +78,31 @@ public class Renderer {
                     2, 3, 0     // Second triangle (bottom-right, bottom-left, top-left)
             };
 
+            FloatBuffer verticesBuffer = BufferUtils.createFloatBuffer(vertices.length);
+            verticesBuffer.put(vertices).flip();
+
+            FloatBuffer texCoordsBuffer = BufferUtils.createFloatBuffer(texCoords.length);
+            texCoordsBuffer.put(texCoords).flip();
+
+            IntBuffer indicesBuffer = BufferUtils.createIntBuffer(indices.length);
+            indicesBuffer.put(indices).flip();
+
             GL46.glBindBuffer(GL46.GL_ARRAY_BUFFER, vertexVBOID);
-            GL46.glBufferData(GL46.GL_ARRAY_BUFFER, vertices, GL46.GL_STATIC_DRAW);
+            GL46.glBufferData(GL46.GL_ARRAY_BUFFER, verticesBuffer, GL46.GL_STATIC_DRAW);
 
             GL46.glBindBuffer(GL46.GL_ARRAY_BUFFER, texCoordVBOID);
-            GL46.glBufferData(GL46.GL_ARRAY_BUFFER, texCoords, GL46.GL_STATIC_DRAW);
+            GL46.glBufferData(GL46.GL_ARRAY_BUFFER, texCoordsBuffer, GL46.GL_STATIC_DRAW);
 
             GL46.glBindBuffer(GL46.GL_ELEMENT_ARRAY_BUFFER, indexVBOID);
-            GL46.glBufferData(GL46.GL_ELEMENT_ARRAY_BUFFER, indices, GL46.GL_STATIC_DRAW);
+            GL46.glBufferData(GL46.GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL46.GL_STATIC_DRAW);
 
             bindTextureId(button.getTextureID());
 
             GL46.glEnableVertexAttribArray(0);
             GL46.glEnableVertexAttribArray(1);
+
+            GL46.glVertexAttribPointer(0, 3, GL46.GL_FLOAT, false, 0, 0);
+            GL46.glVertexAttribPointer(1, 2, GL46.GL_FLOAT, false, 0, 0);
 
             GL46.glDrawElements(GL46.GL_TRIANGLES, 6, GL46.GL_UNSIGNED_INT, 0);
 
@@ -98,6 +115,7 @@ public class Renderer {
 
         Renderer.deleteResources();
     }
+
     private static void createVAO() {
         vaoID = GL46.glGenVertexArrays();
         GL46.glBindVertexArray(vaoID);
@@ -113,6 +131,7 @@ public class Renderer {
         indexVBOID = GL46.glGenBuffers();
         GL46.glBindBuffer(GL46.GL_ELEMENT_ARRAY_BUFFER, indexVBOID);
     }
+
     private static void enableBlending() {
         GL46.glEnable(GL46.GL_BLEND);
         GL46.glBlendFunc(GL46.GL_SRC_ALPHA, GL46.GL_ONE_MINUS_SRC_ALPHA);
@@ -121,8 +140,6 @@ public class Renderer {
     private static void disableBlending() {
         GL46.glDisable(GL46.GL_BLEND);
     }
-
-    //cleanup VAO and VBOS to saves resources when not viewed , like if i am not in the title screen
 
     public static void deleteResources() {
         if (!isTitleScreenDisplayed) {
@@ -133,34 +150,21 @@ public class Renderer {
     private static void deleteVAOAndVBOs() {
         if (vaoID != 0) {
             GL46.glDeleteVertexArrays(vaoID);
-            if (GL46.glGetError() != GL46.GL_NO_ERROR) {
-                // Handle error
-            }
             vaoID = 0;
         }
-
         if (vertexVBOID != 0) {
             GL46.glDeleteBuffers(vertexVBOID);
-            if (GL46.glGetError() != GL46.GL_NO_ERROR) {
-                // Handle error
-            }
             vertexVBOID = 0;
         }
 
-        if (texCoordVBOID != 0) {
-            GL46.glDeleteBuffers(texCoordVBOID);
-            if (GL46.glGetError() != GL46.GL_NO_ERROR) {
-                // Handle error
-            }
-            texCoordVBOID = 0;
-        }
-
-        if (indexVBOID != 0) {
-            GL46.glDeleteBuffers(indexVBOID);
-            if (GL46.glGetError() != GL46.GL_NO_ERROR) {
-                // Handle error
-            }
-            indexVBOID = 0;
-        }
+      if (texCoordVBOID != 0) {
+        GL46.glDeleteBuffers(texCoordVBOID);
+        texCoordVBOID = 0;
     }
+
+    if (indexVBOID != 0) {
+        GL46.glDeleteBuffers(indexVBOID);
+        indexVBOID = 0;
+    }
+}
 }
