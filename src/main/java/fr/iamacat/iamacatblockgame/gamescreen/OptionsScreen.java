709939cdaacb2c580f778cdf9fbeecfbbe17187e
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-05-17 21:57:07 +0200

    Added SKins

--- src/main/java/fr/iamacat/iamacatblockgame/gamescreen/OptionsScreen.java
+++ src/main/java/fr/iamacat/iamacatblockgame/gamescreen/OptionsScreen.java
@@ -8,14 +8,20 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
 public class OptionsScreen implements Screen, InputProcessor {
     private final SpriteBatch batch;
     private final Texture optionsScreenTexture;
-    private final Button vsyncButton;
-    private final Button backButton;
+    private final TextButton backButton;
     private boolean vsyncEnabled;
-    private final OrthographicCamera camera; // Added camera variable
+    private final OrthographicCamera camera;
+    private Stage stage;
+    private CheckBox vsyncCheckbox;
+    private Skin skin;
 
     public OptionsScreen(SpriteBatch batch) {
         this.batch = batch;
@@ -26,21 +32,24 @@ public class OptionsScreen implements Screen, InputProcessor {
         float windowHeight = Gdx.graphics.getHeight();
         float aspectRatio = windowWidth / windowHeight;
 
-        camera = new OrthographicCamera(); // Initialize the camera
-        camera.setToOrtho(false, windowWidth, windowHeight); // Set the camera's viewport
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, windowWidth, windowHeight);
 
-        backButton = new Button("Back", 100, 100, 100, 50,
-                new Texture("textures/optionscreen/button/backbutton.png"));
-
-        vsyncButton = new Button("VSync", 100, 200, 100, 50,
-                new Texture("textures/optionscreen/button/vsyncbutton.png"));
+        backButton = new TextButton("Back", skin);
+        backButton.setPosition(100, 100);
+        backButton.setSize(100, 50);
 
         vsyncEnabled = Gdx.graphics.isContinuousRendering();
 
         Gdx.input.setInputProcessor(this);
-    }
+        stage = new Stage();
+        skin = new Skin(Gdx.files.internal("assets/uis/skins/default/skin/uiskin.json"));
 
-    public void update() {
+        vsyncCheckbox = new CheckBox("VSync", skin);
+        vsyncCheckbox.setPosition(100, 200);
+        vsyncCheckbox.setSize(100, 50);
+        vsyncCheckbox.setChecked(vsyncEnabled);
+        stage.addActor(vsyncCheckbox);
     }
 
     public void renderOptionsScreen() {
@@ -51,38 +60,35 @@ public class OptionsScreen implements Screen, InputProcessor {
 
     public void renderButtons() {
         batch.begin();
-        backButton.draw(batch);
-        vsyncButton.draw(batch);
+        backButton.draw(batch, 1f); // Pass 1f as the second argument to specify full opacity
         batch.end();
     }
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        // Convert screen coordinates to game coordinates
         Vector3 touchPos = new Vector3(screenX, screenY, 0);
-        camera.unproject(touchPos); // Use the camera to unproject the coordinates
+        camera.unproject(touchPos);
 
         float mouseX = touchPos.x;
         float mouseY = touchPos.y;
 
         System.out.println("Clicked at: (" + mouseX + ", " + mouseY + ")");
 
-        if (backButton.isClicked(mouseX, mouseY)) {
+        if (backButton.isPressed()) {
             System.out.println("Back button clicked");
             ((Game) Gdx.app.getApplicationListener()).setScreen(new TitleScreen(batch));
             return true;
         }
 
-        if (vsyncButton.isClicked(mouseX, mouseY)) {
-            System.out.println("VSync button clicked");
-            vsyncEnabled = !vsyncEnabled;
+        if (vsyncCheckbox.isOver()) {
+            System.out.println("VSync checkbox clicked");
+            vsyncEnabled = vsyncCheckbox.isChecked();
             Gdx.graphics.setContinuousRendering(vsyncEnabled);
             return true;
         }
 
         return false;
     }
-
 
     @Override
     public boolean keyDown(int keycode) {
@@ -121,17 +127,20 @@ public class OptionsScreen implements Screen, InputProcessor {
 
     @Override
     public void show() {
-
+        Gdx.input.setInputProcessor(stage);
     }
+
     @Override
     public void render(float delta) {
-        // Render the screen.
         renderOptionsScreen();
         renderButtons();
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
@@ -151,7 +160,7 @@ public class OptionsScreen implements Screen, InputProcessor {
 
     @Override
     public void dispose() {
-        // Dispose of any disposable resources.
         optionsScreenTexture.dispose();
+        stage.dispose();
     }
 }
