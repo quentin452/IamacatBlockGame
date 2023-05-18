1fb8b467ad23c6d941fcc03c6d8789661607cef4
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-05-18 03:13:45 +0200

    Update WorldGeneratorScene.java

--- src/main/java/fr/iamacat/iamacatblockgame/algorythme/chunkalgo/WorldGeneratorScene.java
+++ src/main/java/fr/iamacat/iamacatblockgame/algorythme/chunkalgo/WorldGeneratorScene.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.VertexAttributes;
@@ -13,6 +14,7 @@ import com.badlogic.gdx.graphics.g3d.attributes.FloatAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Matrix4;
 import fr.iamacat.iamacatblockgame.worldgen.core.WorldGenerator;
 
 public class WorldGeneratorScene implements Screen {
@@ -24,6 +26,7 @@ public class WorldGeneratorScene implements Screen {
     private CameraInputController cameraController;
     private WorldGenerator worldGenerator;
     private boolean shouldExit = false;
+
     public boolean shouldExit() {
         return shouldExit;
     }
@@ -31,7 +34,10 @@ public class WorldGeneratorScene implements Screen {
     private void exit() {
         shouldExit = true;
     }
+
     public void create() {
+        System.out.println("Creating WorldGeneratorScene...");
+
         modelBatch = new ModelBatch();
         camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         camera.position.set(10f, 10f, 10f);
@@ -45,6 +51,7 @@ public class WorldGeneratorScene implements Screen {
 
         ModelBuilder modelBuilder = new ModelBuilder();
         modelBuilder.begin();
+
         for (int chunkX = 0; chunkX < chunks.length; chunkX++) {
             for (int chunkY = 0; chunkY < chunks[chunkX].length; chunkY++) {
                 Chunk chunk = chunks[chunkX][chunkY];
@@ -52,40 +59,28 @@ public class WorldGeneratorScene implements Screen {
                     for (int y = 0; y < chunk.getHeight(); y++) {
                         for (int z = 0; z < chunk.getLength(); z++) {
                             float blockHeight = chunk.getBlockHeight(x, y, z);
-                            // Create a cube model instance at the corresponding position and height
-                            Model blockModel = modelBuilder.createBox(1f, 1f, 1f,
-                                    new Material(ColorAttribute.createDiffuse(1f, 1f, 1f, 1f)),
-                                    VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
-                            ModelInstance blockInstance = new ModelInstance(blockModel);
-                            blockInstance.transform.setToTranslation(x, blockHeight, y);
-                            modelBatch.begin(camera); // Begin modelBatch before rendering the block instance
-                            modelBatch.render(blockInstance, environment);
+                            // Create a cube model using the modelBuilder
+                            modelBuilder.part("box_" + x + "_" + y + "_" + z, GL20.GL_TRIANGLES,
+                                            VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal,
+                                            new Material(ColorAttribute.createDiffuse(1f, 1f, 1f, 1f)))
+                                    .box(1f, 1f, 1f);
+                            modelBuilder.node().id = "box_" + x + "_" + y + "_" + z;
+                            modelBuilder.node().translation.set(x, blockHeight, y);
                         }
                     }
                 }
             }
         }
-        modelBuilder.end(); // End the modelBuilder after all blocks have been rendered
 
-        instance = new ModelInstance(model);
+        Model blockModel = modelBuilder.end(); // End the modelBuilder and retrieve the model
+        System.out.println("ModelBuilder ended.");
+
+        instance = new ModelInstance(blockModel); // Create a new ModelInstance using the model
         environment = new Environment();
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
 
         cameraController = new CameraInputController(camera);
         Gdx.input.setInputProcessor(cameraController);
-    }
-
-    public void render() {
-        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT | GL30.GL_DEPTH_BUFFER_BIT);
-
-        cameraController.update();
-        modelBatch.begin(camera);
-        modelBatch.render(instance, environment);
-        modelBatch.end();
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) { // #todo
-            exit();
-        }
     }
 
     @Override
@@ -95,7 +90,17 @@ public class WorldGeneratorScene implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT | GL30.GL_DEPTH_BUFFER_BIT);
 
+        cameraController.update();
+        modelBatch.begin(camera);
+        modelBatch.render(instance, environment);
+        modelBatch.end();
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            exit();
+        }
     }
 
     @Override
@@ -121,5 +126,8 @@ public class WorldGeneratorScene implements Screen {
     public void dispose() {
         modelBatch.dispose();
         model.dispose();
+    }
+
+    public void render() {
     }
 }
